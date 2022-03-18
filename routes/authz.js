588d15939fce5329cd110cadea3c0cdc84bcc5ce
Jared Hanson <jaredhanson@gmail.com>
2022-03-18 11:20:15 -0700

    More validation when creating grant.

--- routes/authz.js
+++ routes/authz.js
@@ -21,37 +21,37 @@ router.get('/consent', csrf(), ensureLoggedIn, function(req, res, next) {
 });
 
 router.post('/consent', csrf(), ensureLoggedIn, function(req, res, next) {
-  console.log('CREATE GRANT');
-  console.log(req.user);
-  console.log(req.body);
+  db.get('SELECT * FROM clients WHERE id = ?', [ req.body.client_id ], function(err, row) {
+    if (err) { return cb(err); }
+    if (!row) { return next(createError(400, 'Unknown client "' + req.body.client_id + '"')); }
 
-
-  db.get('SELECT * FROM grants WHERE user_id = ? AND client_id = ?', [
-    req.user.id,
-    req.body.client_id
-  ], function(err, row) {
-    if (err) { return next(err); }
-    if (!row) {
-      db.run('INSERT INTO grants (user_id, client_id, scope) VALUES (?, ?, ?)', [
-        req.user.id,
-        req.body.client_id,
-        'profile email'
-      ], function(err) {
-        if (err) { return next(err); }
-        var grant = {
-          id: this.lastID,
-        };
-        var url = '/';
-        if (req.session.returnTo) {
-          url = req.session.returnTo;
-          delete req.session.returnTo;
-        }
-        return res.redirect(url);
-      });
-    } else {
-      console.log('UPDATE THE GRANT');
-      console.log(row);
-    }
+    db.get('SELECT * FROM grants WHERE user_id = ? AND client_id = ?', [
+      req.user.id,
+      req.body.client_id
+    ], function(err, row) {
+      if (err) { return next(err); }
+      if (!row) {
+        db.run('INSERT INTO grants (user_id, client_id, scope) VALUES (?, ?, ?)', [
+          req.user.id,
+          req.body.client_id,
+          'profile email'
+        ], function(err) {
+          if (err) { return next(err); }
+          var grant = {
+            id: this.lastID
+          };
+          var url = '/';
+          if (req.session.returnTo) {
+            url = req.session.returnTo;
+            delete req.session.returnTo;
+          }
+          return res.redirect(url);
+        });
+      } else {
+        console.log('UPDATE THE GRANT');
+        console.log(row);
+      }
+    });
   });
 });
 
