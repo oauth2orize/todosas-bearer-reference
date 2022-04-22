3bceed248fe16dd1218a98952f3f0ffff6686595
Jared Hanson <jaredhanson@gmail.com>
2022-04-21 18:23:18 -0700

    Set session cookie name to id.sid.

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
+  name: 'id.sid',
   secret: 'keyboard cat',
   resave: false, // don't save session if unmodified
   saveUninitialized: false, // don't create session until something stored
