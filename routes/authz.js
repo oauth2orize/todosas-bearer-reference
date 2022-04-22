f344a47e21f49e4e8ef753d5d9326c7d3fd40ca5
Jared Hanson <jaredhanson@gmail.com>
2022-04-22 14:32:03 -0700

    Stylistic modifications.

--- routes/authz.js
+++ routes/authz.js
@@ -41,38 +41,45 @@ function fetchGrant(req, res, next) {
 
 var router = express.Router();
 
-router.get('/consent', csrf(), ensureLoggedIn, fetchClient, function(req, res, next) {
-  res.render('consent', {
-    user: req.user,
-    scope: req.query.scope && req.query.scope.split(' '),
-    action: url.parse(req.originalUrl).pathname,
-    csrfToken: req.csrfToken()
+router.get('/consent',
+  csrf(),
+  ensureLoggedIn,
+  fetchClient,
+  function(req, res, next) {
+    res.render('consent', {
+      user: req.user,
+      scope: req.query.scope && req.query.scope.split(' '),
+      action: url.parse(req.originalUrl).pathname,
+      csrfToken: req.csrfToken()
+    });
   });
-});
 
-router.post('/consent', csrf(), ensureLoggedIn, fetchClient, function(req, res, next) {
-  db.run('INSERT INTO grants (user_id, client_id, scope) VALUES (?, ?, ?)', [
-    req.user.id,
-    res.locals.client.id,
-    req.body.scope
-  ], function(err) {
-    if (err) { return cb(err); }
-    var grant = {
-      id: this.lastID,
-      scope: req.body.scope
-    };
-    var to;
-    if (req.session.returnTo) {
-      to = url.parse(req.session.returnTo, true);
-      to.query.grant_id = grant.id;
-      to.query.scope = grant.scope;
-      delete to.search;
-      to = url.format(to);
-      delete req.session.returnTo;
-    }
-    return res.redirect(to || '/');
+router.post('/consent',
+  csrf(),
+  ensureLoggedIn,
+  function(req, res, next) {
+    db.run('INSERT INTO grants (user_id, client_id, scope) VALUES (?, ?, ?)', [
+      req.user.id,
+      req.body.client_id,
+      req.body.scope
+    ], function(err) {
+      if (err) { return cb(err); }
+      var grant = {
+        id: this.lastID,
+        scope: req.body.scope
+      };
+      var to;
+      if (req.session.returnTo) {
+        to = url.parse(req.session.returnTo, true);
+        to.query.grant_id = grant.id;
+        to.query.scope = grant.scope;
+        delete to.search;
+        to = url.format(to);
+        delete req.session.returnTo;
+      }
+      return res.redirect(to || '/');
+    });
   });
-});
 
 router.get('/consent/:grantID', csrf(), ensureLoggedIn, fetchClient, function(req, res, next) {
   res.render('consent', {
